b7a650b4f2218c06a93dfc87b816d76c1751f8e2
Mira Lilleholt Vik <miralv@idun-login2.foreman.hpc.ntnu.no>
2019-04-11 13:34:03 +0200

    adding plots

--- TestRun/CMakeFiles/poisson_parallel.dir/DependInfo.cmake
+++ TestRun/CMakeFiles/poisson_parallel.dir/DependInfo.cmake
@@ -4,19 +4,17 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/mira/Documents/NTNU/TMA4280-P2/poisson_parallel.cpp" "/home/mira/Documents/NTNU/TMA4280-P2/TestRun/CMakeFiles/poisson_parallel.dir/poisson_parallel.cpp.o"
+  "/lustre1/home/miralv/TMA4280-P2/poisson_parallel.cpp" "/lustre1/home/miralv/TMA4280-P2/TestRun/CMakeFiles/poisson_parallel.dir/poisson_parallel.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi"
-  "/usr/lib/x86_64-linux-gnu/openmpi/include"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/mira/Documents/NTNU/TMA4280-P2/TestRun/CMakeFiles/common.dir/DependInfo.cmake"
+  "/lustre1/home/miralv/TMA4280-P2/TestRun/CMakeFiles/common.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
